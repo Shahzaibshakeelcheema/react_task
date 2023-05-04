3ae85fb7881452b5488282be0fc5156091366116
AG-M002 <shahzaibshakeelcheema@gmail.com>
2023-05-04 14:42:06 +0500

    3rd branch with second paragraph

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>This is Appsgenii's AI Bot</h1>
       <p>this is paragraph</p>
+      <p>this is second paragraph on 3rd branch
+      </p>
     </div>
   );
 }
